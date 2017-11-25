ab9cbea37711d3b2bf76f00102722346e2efa210
Iulian Miron <alexandru.iulian.miron@gmail.com>
2017-11-25 19:38:20 +0200

    -

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var router = express.Router();
 var app = express();
 var mongojs = require('mongojs');
 var bodyParser = require('body-parser');
