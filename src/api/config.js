a30b8a016dd21f2d54867088029fd29d49739477
Iulian Miron <alexandru.iulian.miron@gmail.com>
2018-01-23 22:54:10 +0200

    -

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,8 @@
 var DB_BASE_URL     = '127.0.0.1';
-var FE_PORT         = (process.env.PORT && process.env.PORT.trim()) || '3500';
 var DB_PORT         = process.env.DEV_SERVER_PORT || '27017';
-var db              = DB_BASE_URL + ':' + DB_PORT.trim() + '/';
 
 module.exports = {
-	FE_PORT: FE_PORT,
-	db: db
+	FE_PORT: (process.env.PORT && process.env.PORT.trim()) || '3500',
+	db: DB_BASE_URL + ':' + DB_PORT.trim() + '/'
 };
 
